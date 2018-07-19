b85bd84a8b05cb1e09592fcaf49e37e3419065cf
PGS-SOFT\lozog <lozog@pgs-soft.com>
2018-07-19 10:37:01 +0200

    Small update

--- Dsc/Configuration/LocalServer.ps1
+++ Dsc/Configuration/LocalServer.ps1
@@ -3,13 +3,13 @@ Configuration LocalServer {
         Node Local {
 
             File Test {
-                DestinationPath = "D:\TestDsc"
+                DestinationPath = "D:\ADsc"
                 Ensure = "Present"
                 Type = "Directory"
             }
 
             File Test2 {
-                DestinationPath = "D:\TestDsc2"
+                DestinationPath = "D:\ADsc2"
                 Ensure = "Present"
                 Type = "Directory"
             }
